24cf81d587d444206d5c06650f061d8d5cadbbe9
Michael D. Lowis <mike@mdlowis.com>
2018-04-29 21:17:11 -0400

    stubbed out client messages for registrar

--- src/registrar.c
+++ src/registrar.c
@@ -1,16 +1,18 @@
 #include <x11.h>
 #include <unistd.h>
 
-Atom XA_REGISTRAR;
+Atom XA_REGISTRAR, XA_REGISTER, XA_DEREGISTER;
 
 void selclear(XConf* x, XEvent* e) {
     exit(0);
 }
 
-void propnotify(XConf* x, XEvent* e) {
-}
-
 void clientmsg(XConf* x, XEvent* e) {
+    if (XA_REGISTER == e->xclient.message_type) {
+        puts("register");
+    } else if (XA_DEREGISTER == e->xclient.message_type) {
+        puts("dergister");
+    }
 }
 
 int main(int argc, char** argv) {
@@ -18,8 +20,9 @@ int main(int argc, char** argv) {
     x11_init(&x);
     x11_mkwin(&x, 1, 1, PropertyChangeMask);
     XA_REGISTRAR = XInternAtom(x.display, "REGISTRAR", 0);
+    XA_REGISTER = XInternAtom(x.display, "REGISTER", 0);
+    XA_DEREGISTER = XInternAtom(x.display, "DEREGISTER", 0);
     x.eventfns[SelectionClear] = selclear;
-    x.eventfns[PropertyNotify] = propnotify;
     x.eventfns[ClientMessage] = clientmsg;
     if (None == XGetSelectionOwner(x.display, XA_REGISTRAR)) {
         XSetSelectionOwner(x.display, XA_REGISTRAR, x.self, CurrentTime);
