56c11309187d669a66c393b256ee8f2c3cf9dfdd
Michael D. Lowis <mike@mdlowis.com>
2018-05-12 21:17:54 -0400

    reworked registrar protocol

--- src/registrar.c
+++ src/registrar.c
@@ -1,17 +1,21 @@
 #include <x11.h>
 #include <unistd.h>
 
-Atom XA_REGISTRAR, XA_REGISTER, XA_DEREGISTER;
+Atom XA_REGISTRAR, XA_ADD, XA_DEL, XA_OPEN;
 
 void selclear(XConf* x, XEvent* e) {
     exit(0);
 }
 
 void clientmsg(XConf* x, XEvent* e) {
-    if (XA_REGISTER == e->xclient.message_type) {
-        puts("register");
-    } else if (XA_DEREGISTER == e->xclient.message_type) {
-        puts("dergister");
+    if (XA_REGISTRAR != e->xclient.message_type)
+        return;
+    if (XA_ADD == e->xclient.data.l[0]) {
+        puts("add");
+    } else if (XA_DEL == e->xclient.data.l[0]) {
+        puts("del");
+    } else if (XA_OPEN == e->xclient.data.l[0]) {
+        puts("open");
     }
 }
 
@@ -19,9 +23,10 @@ int main(int argc, char** argv) {
     XConf x;
     x11_init(&x);
     x11_mkwin(&x, 1, 1, PropertyChangeMask);
-    XA_REGISTRAR = XInternAtom(x.display, "REGISTRAR", 0);
-    XA_REGISTER = XInternAtom(x.display, "REGISTER", 0);
-    XA_DEREGISTER = XInternAtom(x.display, "DEREGISTER", 0);
+    XA_REGISTRAR = XInternAtom(x.display, "TIDE_REGISTRAR", 0);
+    XA_ADD = XInternAtom(x.display, "ADD", 0);
+    XA_DEL = XInternAtom(x.display, "DEL", 0);
+    XA_OPEN = XInternAtom(x.display, "OPEN", 0);
     x.eventfns[SelectionClear] = selclear;
     x.eventfns[ClientMessage] = clientmsg;
     if (None == XGetSelectionOwner(x.display, XA_REGISTRAR)) {
