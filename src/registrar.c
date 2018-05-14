88247484d46744b26acb32e15c0fad77dee65908
Michael D. Lowis <mike@mdlowis.com>
2018-05-13 22:48:58 -0400

    implemented rudimentary window management

--- src/registrar.c
+++ src/registrar.c
@@ -1,7 +1,37 @@
 #include <x11.h>
 #include <unistd.h>
 
+typedef struct TWindow {
+    struct TWindow* next;
+    Window win;
+} TWindow;
+
 Atom XA_REGISTRAR, XA_ADD, XA_DEL, XA_OPEN;
+TWindow* Windows = NULL;
+
+static void win_add(Window id) {
+    TWindow* win = calloc(1, sizeof(TWindow));
+    win->win = id;
+    win->next = Windows;
+    Windows = win;
+}
+
+static void win_del(Window id) {
+    if (!Windows) return;
+    if (Windows->win == id) {
+        TWindow* deadite = Windows;
+        Windows = deadite->next;
+        free(deadite);
+    } else {
+        TWindow* w = Windows;
+        for (;w && w->next && (w->next->win != id); w = w->next);
+        if (w && w->next) {
+            TWindow* deadite = w->next;
+            w->next = deadite->next;
+            free(deadite);
+        }
+    }
+}
 
 void selclear(XConf* x, XEvent* e) {
     exit(0);
@@ -10,13 +40,12 @@ void selclear(XConf* x, XEvent* e) {
 void clientmsg(XConf* x, XEvent* e) {
     if (XA_REGISTRAR != e->xclient.message_type)
         return;
-    if (XA_ADD == e->xclient.data.l[0]) {
-        puts("add");
-    } else if (XA_DEL == e->xclient.data.l[0]) {
-        puts("del");
-    } else if (XA_OPEN == e->xclient.data.l[0]) {
+    if (XA_ADD == e->xclient.data.l[0])
+        win_add(e->xclient.window);
+    else if (XA_DEL == e->xclient.data.l[0])
+        win_del(e->xclient.window);
+    else if (XA_OPEN == e->xclient.data.l[0])
         puts("open");
-    }
 }
 
 int main(int argc, char** argv) {
